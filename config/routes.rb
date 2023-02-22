2a3fcf989210563d8ff94865c16436b42c42d5a0
Arlen Burroughs <arlen.bu@gmail.com>
2023-02-22 12:25:30 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,24 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
   resources :messages
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "login", to: "sessions#create"
+  get "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
+
+  get 'static_pages/home'
+  root "static_pages#home"
+
+  get '/messages', to: 'messages#index'
+  post '/messages', to: 'messages#create'
+  get '/messages/:id', to: 'messages#show'
+
+  post "/new_message", to: "messages#submit"
+  get "/new_message", to: "messages#new"
+
+
+  post "/user", to: "user#submit_user"
+  get "/signup", to: "user#new"
+
+  get "/healthcheck", to: "healthcheck#index"
 end
